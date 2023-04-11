fb24f9536c7a984df58d62fc28d992a2945fea66
bramvanderzee <bram.vdzee@gmail.com>
2023-04-11 13:48:39 +0200

    added model saving

--- start_nb.sh
+++ start_nb.sh
@@ -1,2 +1,2 @@
- docker pull tensorflow/tensorflow:latest  # Download latest stable image
- docker run -it -p 8888:8888 tensorflow/tensorflow:latest-jupyter  # Start Jupyter server 
+ docker pull tensorflow/tensorflow:latest-gpu-jupyter  # Download latest stable image
+ docker run -it --gpus all -p 8888:8888 -v /home/bram/repos/handwriting:/home/bram/repos/handwriting tensorflow/tensorflow:latest-gpu-jupyter
